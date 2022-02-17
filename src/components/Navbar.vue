c43e400967fc3c6c624d46aff2942af8ce602297
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-17 01:19:54 -0300

    Creacion de componente navbar para pantalla principal

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-toolbar>
+      <v-toolbar-title>
+          Gestión de Alumnos
+      </v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
