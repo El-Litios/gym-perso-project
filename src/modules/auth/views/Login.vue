49c0a4626236775a7e932e3c2282e5fb386f996b
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-21 19:48:31 -0300

    Formulario de logeo

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
       <Navbar />
-      <h1>Login</h1>
+      <br>
+        <v-container>
+        <v-row>
+            <v-col cols="12"  align="center" justify="center">
+                <v-text-field
+                    label="Ingrese su correo"
+                    :rules="rules"
+                    hide-details="auto"
+                ></v-text-field>
+            </v-col>
+            <v-col cols="12"  align="center" justify="center">
+                <v-text-field
+                    type="password"
+                    label="Ingrese su contraseña"
+                ></v-text-field>
+            </v-col>
+            <v-col cols="12" align="center" justify="center">
+                <v-btn elevation="7" x-large block >Ingresar</v-btn>
+            </v-col>
+        </v-row>
+        </v-container>
   </div>
 </template>
 
@@ -11,6 +31,15 @@ import Navbar from '../components/Navbar.vue'
 export default {
     components: {
         Navbar
+    },
+
+    data(){
+        return {
+            rules: [
+                value => !!value || 'Es requerido.',
+                value => /.+@.+/.test(value) || 'De ser un email válido',
+            ],
+        }
     }
 }
 </script>
