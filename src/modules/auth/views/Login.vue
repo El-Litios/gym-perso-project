65950b21eaa5d15c763d055572e5916bfff1c011
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-24 23:35:23 -0300

    Importaciones LazyLoad de componentes

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -18,19 +18,20 @@
                 ></v-text-field>
             </v-col>
             <v-col cols="12" align="center" justify="center">
-                <v-btn elevation="7" x-large block >Ingresar</v-btn>
+                <v-btn elevation="7" x-large block :to="{ name: 'StudentsList'  }">Ingresar</v-btn>
             </v-col>
         </v-row>
         </v-container>
+        <Footer />
   </div>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue'
 
 export default {
     components: {
-        Navbar
+        Navbar: () => import('../components/Navbar.vue'),
+        Footer: () => import('../components/Footer.vue'),
     },
 
     data(){
