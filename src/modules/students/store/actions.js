c0f5d4081c1c6843fca0483663aa7a81e8389a70
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-24 23:37:17 -0300

    Crea accion de prueba para obtener data desde firestore

--- src/modules/students/store/actions.js
+++ src/modules/students/store/actions.js
@@ -1,3 +1,13 @@
-/* export const Actions =async ({commit}) => {
+import { db } from '../../../firebase'
 
-} */
+
+export const getStudents = async ({/* commit */ rootState}) => {
+    const students = []
+    await db.collection(`${rootState.auth.user}/Userdata/students`).get()
+        .then(res => {
+            res.forEach(doc => {
+                console.log(doc.data());
+            });
+        })
+        .catch(err => console.log(err))
+} 
