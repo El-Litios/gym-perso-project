a75c63c5343a47f93bfba53aee27911a502fa492
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-24 23:31:04 -0300

    Agrega footer a vista de login

---
+++ src/modules/auth/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-footer fixed>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>Todos los derechos reservados.</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
