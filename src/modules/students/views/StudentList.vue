5522896b31b957ccbad951c2a7dcb0f9846b5dc0
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-24 23:38:53 -0300

    Creacion de vista para el listado de estudiantes, agrega lazy load de components

---
+++ src/modules/students/views/StudentList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+        <Navbar />
+        
+        <h1>Lista</h1>
+
+        <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+    components: {
+        Navbar: () => import('../components/Navbar.vue'),
+        Footer: () => import('../components/Footer.vue'),
+    },
+    created() {
+        this.$store.dispatch('students/getStudents')
+    }
+}
+</script>
+
+<style>
+
+</style>
