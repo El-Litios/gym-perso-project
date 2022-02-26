529a1c9cda567b1c451d415fa0de7e7de38024b2
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-25 21:22:31 -0300

    Prueba de listado de data a traves del mapeo de vuex

--- src/modules/students/views/StudentList.vue
+++ src/modules/students/views/StudentList.vue
@@ -4,11 +4,15 @@
         
         <h1>Lista</h1>
 
+        <h1 v-for="item in students" :key="item.id">{{item.name}}</h1>
+
         <Footer />
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+
 export default {
     components: {
         Navbar: () => import('../components/Navbar.vue'),
@@ -16,6 +20,10 @@ export default {
     },
     created() {
         this.$store.dispatch('students/getStudents')
+    },
+
+    computed: {
+        ...mapState('students', ['students'])
     }
 }
 </script>
