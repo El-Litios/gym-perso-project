be3344e5333a14126db4de7dad261670ef00ce49
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-26 23:10:16 -0600

    Agrega tabla a la lista

--- src/modules/students/views/StudentList.vue
+++ src/modules/students/views/StudentList.vue
@@ -3,8 +3,42 @@
         <Navbar />
         
         <h1>Lista</h1>
-
-        <h1 v-for="item in students" :key="item.id">{{item.name}}</h1>
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                    <tr>
+                    <th class="text-left">
+                        Rut
+                    </th>
+                    <th class="text-left">
+                        Nombre
+                    </th>
+                    <th class="text-left">
+                        Editar
+                    </th>
+                    <th class="text-left">
+                        Eliminar
+                    </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                    v-for="item in students"
+                    :key="item.id"
+                    >
+                    <td>{{ item.rut }}</td>
+                    <td>{{ item.name }} {{ item.lastname }}</td>
+                    <td>
+                        <v-btn color="yellow accent-3" >Editar</v-btn>
+                    </td>
+                    <td>
+                        <v-btn color="red accent-3" >Eliminar</v-btn>
+                    </td>
+                    
+                    </tr>
+                </tbody>
+            </template>
+        </v-simple-table>
 
         <Footer />
   </div>
