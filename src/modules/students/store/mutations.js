f50c65dacd75e56311f25e4af5c751f976d98aaa
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-02 23:03:45 -0300

    Savar estudiantes de la lista, luego de ser eliminados

--- src/modules/students/store/mutations.js
+++ src/modules/students/store/mutations.js
@@ -1,3 +1,7 @@
 export const setStudents =  (state, payload) => {
     state.students = [...state.students, ...payload]
 } 
+
+export const unsetStudentAfterDelete = (state, payload) => {
+    state.students = state.students.filter(s => s.id !== payload)
+}
