cc97e2b70aed0ada7a8a1c8dd437368c8c4f9930
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-02 23:03:11 -0300

    Formulario para agregar alumnos

--- src/modules/students/components/Modal.vue
+++ src/modules/students/components/Modal.vue
@@ -1,52 +1,132 @@
 <template>
     <div class="text-center">
-    <v-dialog
-      v-model="dialog"
-      width="500"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="green lighten-2"
-          x-small
-          v-bind="attrs"
-          v-on="on"
-        >
-          Agregar
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-card-title class="text-h5 grey lighten-2">
-          Privacy Policy
-        </v-card-title>
-
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
+      <v-dialog
+        v-model="dialog"
+        width="400"
+      >
+        <template v-slot:activator="{ on, attrs }">
           <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
+            color="green lighten-2"
+            x-small
+            v-bind="attrs"
+            v-on="on"
           >
-            I accept
+            Agregar
           </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+        </template>
+
+        <v-card>
+          <v-card-title class="text-h5 white lighten-2">
+            Formulario
+          </v-card-title>
+
+          <form @submit.prevent="createStudent(student)">
+
+            <v-card-text>
+            
+              <!-- Rut -->
+              <v-text-field
+                label="Rut"
+                :rules="rutRules"
+                hide-details="auto"
+                v-model="student.rut"
+              ></v-text-field>
+
+              <v-spacer></v-spacer>
+
+              <!-- Name -->
+              <v-text-field
+                label="Nombre"
+                :rules="nameRules"
+                hide-details="auto"
+                v-model="student.name"
+              ></v-text-field>
+
+              <v-spacer></v-spacer>
+
+              <!-- Father Lastname -->
+              <v-text-field
+                label="Apellido Paterno"
+                :rules="flRules"
+                hide-details="auto"
+                v-model="student.flname"
+              ></v-text-field>
+
+              <v-spacer></v-spacer>
+
+              <!-- Mother Lastname -->
+              <v-text-field
+                label="Apellido Materno"
+                :rules="mlRules"
+                hide-details="auto"
+                v-model="student.mlname"
+              ></v-text-field>
+
+              <v-spacer></v-spacer>
+              
+              <!-- Phone -->
+              <v-text-field
+                label="Teléfono"
+                :rules="phoneRules"
+                hide-details="auto"
+                v-model="student.phone"
+              ></v-text-field>
+
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                type="submit"
+                color="primary"
+                text
+                @click="dialog = false"
+              >
+                I accept
+              </v-btn>
+            </v-card-actions>
+
+          </form>
+
+        </v-card>
+      </v-dialog>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
-    data(){
-        return {
-            dialog: false
-        }
-    }
+  data(){
+      return {
+          dialog: false,
+          student: {},
+
+          //RULES
+          rutRules: [
+            value => !!value || 'Debe ingresar el rut.',
+          ],
+          nameRules: [
+            value => !!value || 'Debe ingresar el nombre.',
+            value => (value || '').length <= 25 || 'Máximo 25 caracteres',
+          ],
+          flRules: [
+            value => !!value || 'Debe ingresar el apellido paterno.',
+            value => (value || '').length <= 25 || 'Máximo 25 caracteres',
+          ],
+          mlRules: [
+            value => !!value || 'Debe ingresar el apellido materno.',
+            value => (value || '').length <= 25 || 'Máximo 25 caracteres',
+          ],
+          phoneRules: [
+            value => !!value || 'Debe ingresar el telefono.'
+          ]
+      }
+  },
+
+  methods: {
+    ...mapActions('students', ['createStudent'])
+  }
 }
 </script>
