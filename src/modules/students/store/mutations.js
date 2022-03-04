fc97f30cd7e09910608265afabd22ec5c05c0128
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-04 00:18:13 -0300

    Mutacion para setear detalles

--- src/modules/students/store/mutations.js
+++ src/modules/students/store/mutations.js
@@ -1,7 +1,12 @@
 export const setStudents =  (state, payload) => {
-    state.students = [...state.students, ...payload]
+    state.students = payload
 } 
 
 export const unsetStudentAfterDelete = (state, payload) => {
     state.students = state.students.filter(s => s.id !== payload)
+    state.details = []
 }
+
+export const setDetailsByStudentId = (state, payload) => {
+    state.details = payload
+} 
