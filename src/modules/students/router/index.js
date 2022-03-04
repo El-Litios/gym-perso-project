e8faf8f439b16b6f249c7c899d21276a4004bc5e
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-04 00:17:35 -0300

    Agrega ruta para mostrar los detalles de los estudiantes

--- src/modules/students/router/index.js
+++ src/modules/students/router/index.js
@@ -5,6 +5,16 @@ export default {
             path: 'studentlist',
             name: 'StudentsList',
             component: () => import(/* webpackChunkName: 'studentlist' */ '../views/StudentList.vue')
+        },
+        {
+            path: ':id',
+            name: 'StudentDetails',
+            component: () => import(/* webpackChunkName: 'studentdetail' */ '../views/StudentDetail.vue'),
+            props: ( route ) => {
+                return {
+                    id: route.params.id
+                }
+            }
         }
     ]
 }
