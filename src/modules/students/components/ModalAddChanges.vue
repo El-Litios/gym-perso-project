2f7f3bfcaf53c062de591136a828f636bf8e5603
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-06 16:59:23 -0600

    Agrega modal con formulario para ingresar avances de estudiantes

---
+++ src/modules/students/components/ModalAddChanges.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="text-center">
+      <v-dialog
+        v-model="dialog"
+        width="400"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="green lighten-2"
+            x-small
+            v-bind="attrs"
+            v-on="on"
+          >
+            Agregar
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="text-h5 white lighten-2">
+            Formulario
+          </v-card-title>
+
+          <v-form @submit.prevent="" lazy-validation ref="form" v-model="valid">
+
+            <v-card-text>
+            
+              <!-- Height -->
+              <v-text-field
+                label="Estatura"
+                :rules="heightRules"
+                hide-details="auto"
+                v-model="details.height"
+                type="number"
+                step=".1"
+              ></v-text-field>
+
+              <v-spacer></v-spacer>
+
+              <!-- Weight -->
+              <v-text-field
+                label="Peso"
+                :rules="weigth"
+                hide-details="auto"
+                v-model="details.weight"
+              ></v-text-field>
+
+              <v-spacer></v-spacer>
+              
+              <!-- Birthdate -->
+              <v-text-field
+                label="Fecha de Hoy"
+                :rules="bdRules"
+                hide-details="auto"
+                v-model="details.date"
+                type="date"
+              ></v-text-field>
+
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                type="submit"
+                color="primary"
+                text
+                @click="validate"
+                :disabled="!valid"
+              >
+                Agregar Cambios
+              </v-btn>
+            </v-card-actions>
+
+          </v-form>
+
+        </v-card>
+      </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+          details: {},
+          dialog: false,    
+          valid: true,
+
+          //RULES
+          heightRules: [
+            value => !!value || 'Debe ingresar el rut.',
+          ],
+          nameRules: [
+            value => !!value || 'Debe ingresar el nombre.',
+            value => (value || '').length <= 25 || 'Máximo 25 caracteres',
+          ],
+          flRules: [
+            value => !!value || 'Debe ingresar el apellido paterno.',
+            value => (value || '').length <= 25 || 'Máximo 25 caracteres',
+          ],
+          mlRules: [
+            value => !!value || 'Debe ingresar el apellido materno.',
+            value => (value || '').length <= 25 || 'Máximo 25 caracteres',
+          ],
+          bdRules: [
+            value => !!value || 'Debe ingresar la fecha de nacimiento',
+
+          ],
+          phoneRules: [
+            value => !!value || 'Debe ingresar el telefono.',
+            value => (value || '').length > 8 && (value || '').length < 10 || 'Debe ingresar 9 dígitos'
+          ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
