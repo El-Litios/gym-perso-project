a811e02414970e3f280882758d728fee2f4bcf8c
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-17 00:07:09 -0600

    Se agregan el vmodel para cada input del formulario junto a las reglas de negocio

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -7,18 +7,21 @@
             <v-col cols="12"  align="center" justify="center">
                 <v-text-field
                     label="Ingrese su correo"
-                    :rules="rules"
+                    :rules="emailRules"
                     hide-details="auto"
+                    v-model="user.email"
                 ></v-text-field>
             </v-col>
             <v-col cols="12"  align="center" justify="center">
                 <v-text-field
                     type="password"
                     label="Ingrese su contraseña"
+                    v-model="user.pass"
+                    :rules="passRules"
                 ></v-text-field>
             </v-col>
             <v-col cols="12" align="center" justify="center">
-                <v-btn elevation="7" x-large block :to="{ name: 'StudentsList'  }">Ingresar</v-btn>
+                <v-btn elevation="7" x-large block @click="loginUser(user)">Ingresar</v-btn>
             </v-col>
         </v-row>
         </v-container>
@@ -27,6 +30,7 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 
 export default {
     components: {
@@ -36,11 +40,21 @@ export default {
 
     data(){
         return {
-            rules: [
-                value => !!value || 'Es requerido.',
+            emailRules: [
+                value => !!value || 'Debe ingresar su correo.',
                 value => /.+@.+/.test(value) || 'De ser un email válido',
             ],
+            passRules: [
+                value => !!value || 'Es requerido.',
+                value => (value || '').length >= 6 || 'Minimo 6 caracteres'
+            ],
+
+            user: {}
         }
+    },
+
+    methods: {
+        ...mapActions('auth', ['loginUser'])
     }
 }
 </script>
