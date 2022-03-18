663053b66dd531c29ecec5316db6f5e4efdd6a02
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-18 16:21:57 -0300

    Agrega funcion de logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,29 @@
 <template>
   <v-toolbar>
       <v-toolbar-title>
-          Gestión de Alumnos
+          Gestion de Alumnos
       </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        v-if="userExists"
+        color="primary"
+        text
+        @click="logoutUser"
+      >
+        Cerrar Sesión
+      </v-btn>
   </v-toolbar>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
 export default {
+  methods: {
+    ...mapActions('auth', ['logoutUser']),
+  },
 
+  computed: {
+    ...mapGetters('auth', ['userExists'])
+  }
 }
 </script>
-
-<style>
-
-</style>
