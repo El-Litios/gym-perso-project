321b517401ba22b8b850ce4dec8111e3c8dd7050
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-18 16:21:48 -0300

    Agrega la funcion que observa los cambios de usuario para su deteccion y asi evitar entrar a rutas protegidas

--- src/main.js
+++ src/main.js
@@ -3,12 +3,26 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import {authen} from './firebase'
+
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+authen.onAuthStateChanged(user => {
+  if (user) {
+    const userDetection = {
+      email: user.email,
+      id: user.uid
+    }
+    store.dispatch('auth/detectUser', userDetection)
+  }else{
+    console.log(user);
+    store.dispatch('auth/detectUser', user)
+  }
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount('#app')
+})
