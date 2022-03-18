03aa2757cf0b166ac43514e3f0afd6e290412c6f
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-18 16:25:56 -0300

    Agrega funcion para requerir autenticacion del usuario

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import AuthRoutes from '../modules/auth/router'
 import StudentsRoutes from '../modules/students/router'
-
+import { authen } from '../firebase'
 
 Vue.use(VueRouter)
 
@@ -18,12 +18,29 @@ const routes = [
   },
   {
     path: '/students',
+    meta: { requiresAuthentication: true },
     ...StudentsRoutes
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuthentication)) {
+    
+    const user = authen.currentUser
+    
+    if(!user){
+      next({ path: '/'} )
+    }else{
+      next()
+    }
+
+  }else{
+    next()
+  }
 })
 
 export default router
