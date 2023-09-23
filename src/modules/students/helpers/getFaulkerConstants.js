f613ab4b11e74e1fcb7202dc08ab9a5b75fc2087
El-Litios <carlosguzmanrojas00@gmail.com>
2023-09-23 19:16:58 -0300

    added faulkner constant by gender

--- src/modules/students/helpers/getFaulkerConstants.js
+++ src/modules/students/helpers/getFaulkerConstants.js
@@ -1,8 +1,14 @@
 const getFaulkerConstants = (gender) => {
     if (gender === 'Masculino') {
-        
+        return {
+          constant_1: 0.153,
+          constant_2: 5.783
+        }
       }else{
-        
+        return {
+          constant_1: 0.219,
+          constant_2: 7.9
+        }
       }
 }
 
